183b9f8a81ce2a03a1a60db3c4261d21af1b196c
Ian Irving <ian@falsepositives.com>
2013-03-06 14:18:04 -0500

    depot A2 add product table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Work::Application.config.secret_key_base = '111e69eea9be79550f84f80fad47e0634bd3958a45c47473cddba5d0175137cda91b906efdabdde32fb174efea0aa5faab9c38a8530c2e5717bda6d62700484c'
