3dfd3a2001efbdd9257c56a61c186b50a3c1631d
Ian Irving <ian@falsepositives.com>
2013-03-12 14:07:33 -0400

    added cars and line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true, :length => 10..160
 
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_items
+
+
 
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
@@ -11,6 +16,17 @@ class Product < ActiveRecord::Base
 
 	def self.latest
 		Product.order('updated_at').last
+	end
+
+	private
+
+	def ensure_not_referenced_by_any_line_items
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Work::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
 
