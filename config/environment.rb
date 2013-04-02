f83f1fbca74488bdac79749d30565d52c66b2775
Ian Irving <ian@falsepositives.com>
2013-04-02 17:20:05 -0400

    port value in quotes

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Work::Application.initialize!
 
 Work::Application.configure do
-	config.action_mailer.deliver_method = :smtp
+	config.action_mailer.delivery_method = :smtp
 
 	config.action_mailer.smtp_settings = {
 		address:       			"smtp.gmail.com",
-		port:          			587,
+		port:          			"587",
 		domain:          		"domain.of.sender.net",
 		authentication:   		"plain",
 		user_name:   			"dave",
