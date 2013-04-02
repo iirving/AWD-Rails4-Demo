b48dbdfdebed886250a478e55e1b4c1a79e3e981
Ian Irving <ian@falsepositives.com>
2013-04-02 15:41:40 -0400

    setting up email config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 Work::Application.initialize!
+
+Work::Application.configure do
+	config.action_mailer.deliver_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		address:       			"smtp.gmail.com",
+		port:          			587,
+		domain:          		"domain.of.sender.net",
+		authentication:   		"plain",
+		user_name:   			"dave",
+		password:   		 	"secret",
+		enable_startitls_auto: 	true
+
+	}
+
+end
