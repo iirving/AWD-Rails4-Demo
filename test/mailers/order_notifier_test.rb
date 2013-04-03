6f1553d9cfa736d337c85778e8685d3fd750a898
Ian Irving <ian@falsepositives.com>
2013-04-03 13:19:00 -0400

    basic tests for order mailer received and shipped

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Store Confirmation", mail.subject
+    assert_equal ["Dave@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match /Thank you for your order/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Store Order Shipped!", mail.subject
+    assert_equal ["Dave@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match /This is just to let you know that we've shipped your recent order/ , mail.body.encoded
   end
 
 end
