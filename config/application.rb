6e3a5fd42bc77e01a381faac7254a9acee823e3b
Ian Irving <ian@falsepositives.com>
2013-04-10 12:57:36 -0400

    more trying to get assets working on heroku :(

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Work
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = true
+    config.assets.initialize_on_precompile=false
   end
 end
